aa14ab9e62cf4ae154c5dbb3b534c0c5d1bc6fa9
forwhfang <945338423@qq.com>
2019-07-24 10:24:42 +0800

    完成 detailPage 中的 detailContent

---
+++ src/components/DetailExtend.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="extend" v-if="isVisible === true">In extend</div>
+</template>
+
+<script>
+export default {
+  name: 'DetailExtend',
+  data: function () {
+    return {
+      isVisible: false
+    }
+  }
+}
+</script>
+
+<style scoped lang="less">
+.extend {
+  background: white;
+}
+</style>
---
+++ src/views/Detail.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="detail">
+    <div class="content">
+      <DetailContent />
+    </div>
+    <div class="extend">
+      <DetailExtend />
+    </div>
+  </div>
+</template>
+
+<script>
+import DetailContent from '@/components/DetailContent.vue'
+import DetailExtend from '@/components/DetailExtend.vue'
+
+export default {
+  name: 'detail',
+  components: {
+    DetailContent,
+    DetailExtend
+  }
+}
+</script>
+
+<style scoped lang="less">
+.detail {
+  height: 100%;
+  width: 100%;
+  background: cornflowerblue;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+
+.content {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+}
+
+.extend {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+}
+</style>
